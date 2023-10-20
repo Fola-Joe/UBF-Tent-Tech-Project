6720cd7376bcf8893930cce1ac1ee8463f7b4123
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-10-20 12:39:41 +0100

    Started with sign up page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,7 +3,25 @@
 export default function SignUp() {
     return (
         <div>
-
+            <img src="./ubf_logo6.png" alt="ubf logo" />
+            <section>
+                <h1>Sign Up</h1>
+                <h2>Create an account</h2>
+                <form action="#">
+                    <div className="full-name">
+                        <label htmlFor="name">Full Name</label>
+                        <input type="text" name="name" placeholder="Enter full name" />
+                    </div>
+                    <div className="address">
+                        <label htmlFor="lone-address">Email Address</label>
+                        <input type="text" name="lone-address" placeholder="Enter email address" />
+                    </div>
+                    <div className="password">
+                        <label htmlFor="name">Password</label>
+                        <input type="text" name="password" placeholder="Create Password" />
+                    </div>
+                </form>
+            </section>
         </div>
     )
 }
